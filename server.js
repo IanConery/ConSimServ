cbadf2236852c43752802396c7243bb3742ae537
Ian Conery <ianconery@gmail.com>
2016-04-17 20:26:48 -0700

    noticed I needed to commit

--- server.js
+++ server.js
@@ -38,5 +38,5 @@ var server = app.listen(3000, function () {
   var host = server.address().address;
   var port = server.address().port;
 
-//  console.log('Example app listening on port %s', port);
+  console.log('Example app listening on port %s and host %s', port, host);
 });
